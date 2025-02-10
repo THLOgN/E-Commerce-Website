f58d3e8a59e15d142180a0a9ddf8e9f4b7357885
THLOgN <102423409+THLOgN@users.noreply.github.com>
2025-02-10 08:06:50 +0700

    update backend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import { connectDB } from './lib/db.js';
+
+import authRoutes from './routes/auth.route.js';
+import productRoutes from './routes/product.route.js'; 
+import cartRoutes from './routes/cart.route.js'; 
+import couponRoutes from "./routes/coupon.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes); // Add this line for product routes
+app.use("/api/cart", cartRoutes); // Add this line for user routes
+app.use("/api/coupons", couponRoutes);
+
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:' + PORT);
+  connectDB();
+});
+//zZTQkhvJdo3Flsir
+
