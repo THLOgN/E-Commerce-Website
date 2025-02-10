cbd9782e28ce174d78d3a7e99a5417ab8622ea1a
THLOgN <102423409+THLOgN@users.noreply.github.com>
2025-02-10 09:56:40 +0700

    update backend-admin-analytic

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js'; 
 import cartRoutes from './routes/cart.route.js'; 
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
 
 dotenv.config();
 
@@ -18,9 +21,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes); // Add this line for product routes
-app.use("/api/cart", cartRoutes); // Add this line for user routes
+app.use("/api/products", productRoutes); 
+app.use("/api/cart", cartRoutes); 
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
