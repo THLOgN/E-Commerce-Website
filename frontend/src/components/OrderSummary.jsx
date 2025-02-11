996490ac769238597693a647352fc6b153b75d90
THLOgN <102423409+THLOgN@users.noreply.github.com>
2025-02-11 10:09:42 +0700

    update stripe

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51QqmDTBCApC1Zz2IVauJZWk4jTok4eGdxs9bc8h4FMpXBg8rXQC2y8f82dQe6ed4xw1HoxGvbPxbLqjhSDdW4vqq00siWzxHjj"
 );
 
 const OrderSummary = () => {
